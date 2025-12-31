af824c64f0d2d36e305e7ee0b38e717454623420
RafatMeraz <razhossain201@gmail.com>
2025-12-31 23:05:06 +0600

    Add fcm service

--- live_score_app/lib/main.dart
+++ live_score_app/lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:live_score_app/fcm_service.dart';
 import 'package:live_score_app/home_screen.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  await FcmService.initialize();
+
+  print(await FcmService.getToken());
+
+  FcmService.listenTokenOnChange();
 
   runApp(const LiveScoreApp());
 }
