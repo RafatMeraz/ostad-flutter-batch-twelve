b1804dfc4ced192854f143d2ebe55ce897796bc4
RafatMeraz <razhossain201@gmail.com>
2026-02-20 22:46:44 +0600

    Add wish list screen ui

--- crafty_bay/lib/features/shared/presentation/screens/main_nav_holder_screen.dart
+++ crafty_bay/lib/features/shared/presentation/screens/main_nav_holder_screen.dart
@@ -1,3 +1,4 @@
+import 'package:crafty_bay/features/wishlist/presentation/screens/wish_list_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -21,7 +22,7 @@ class _MainNavHolderScreenState extends State<MainNavHolderScreen> {
     HomeScreen(),
     CategoryListScreen(),
     SizedBox(),
-    SizedBox(),
+    WishListScreen(),
   ];
 
   @override
---
+++ crafty_bay/lib/features/wishlist/presentation/screens/wish_list_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import '../../../shared/presentation/widgets/product_card.dart';
+
+class WishListScreen extends StatefulWidget {
+  const WishListScreen({super.key});
+
+  static const String name = '/wish-list';
+
+  @override
+  State<WishListScreen> createState() => _WishListScreenState();
+}
+
+class _WishListScreenState extends State<WishListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Wishlist')),
+      body: GridView.builder(
+        itemCount: 10,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          mainAxisSpacing: 4,
+        ),
+        itemBuilder: (context, index) {
+          return FittedBox(child: ProductCard());
+        },
+      ),
+    );
+  }
+}
